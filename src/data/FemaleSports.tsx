b35659e3a2dc960cc5bf8eaa770256d9553966a4
seniorQAautomationEngineer <oleksii.lavrenin@gmail.com>
2024-11-30 21:25:50 -0500

    Added all sports and typehead.

---
+++ src/data/FemaleSports.tsx
@@ -0,0 +1,29 @@
+const femaleSports = [
+    "Acrobatics & Tumbling",
+    "Basketball",
+    "Beach Volleyball",
+    "Bowling",
+    "Cross Country",
+    "Diving",
+    "Equestrian",
+    "Fencing",
+    "Field Hockey",
+    "Golf",
+    "Gymnastics",
+    "Ice Hockey",
+    "Lacrosse",
+    "Rifle",
+    "Rowing",
+    "Skiing",
+    "Soccer",
+    "Softball",
+    "Swimming",
+    "Tennis",
+    "Track & Field (Indoor)",
+    "Track & Field (Outdoor)",
+    "Triathlon",
+    "Volleyball"
+  ];
+  
+  export default femaleSports;
+  
---
+++ src/data/MaleSports.tsx
@@ -0,0 +1,30 @@
+const maleSports = [
+    "Baseball",
+    "Basketball",
+    "Bowling",
+    "Cheerleading",
+    "Cross Country",
+    "Diving",
+    "ESports",
+    "Fencing",
+    "Football",
+    "Golf",
+    "Gymnastics",
+    "Ice Hockey",
+    "Lacrosse",
+    "Rifle",
+    "Rowing",
+    "Rugby",
+    "Skiing",
+    "Soccer",
+    "Swimming",
+    "Tennis",
+    "Track & Field",
+    "Triathlon",
+    "Volleyball",
+    "Water Polo",
+    "Wrestling"
+  ];
+  
+  export default maleSports;
+  
